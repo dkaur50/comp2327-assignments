0514fb333891c803d2b47566c35380ef533ddeec
Divjot Kaur <dkaur50@academic.rrc.ca>
2026-03-10 16:36:24 -0500

    feat: The ServiceChargeStrategy has been developed.

---
+++ patterns/strategy/service_charge_strategy.py
@@ -0,0 +1,15 @@
+"""Module Documentation"""
+
+__author__ = "Divjot Kaur"
+__version__ = "1.0.0"
+
+from abc import ABC, abstractmethod
+
+from bank_account.bank_account import BankAccount
+
+class ServiceChargeStrategy(ABC):
+    """This class has been created to the service charge strategy."""
+
+    def calculate_service_charges(self, account: BankAccount,
+                                  BASE_SERVICE_CHARGE: float= 0.50):
+
