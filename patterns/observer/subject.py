6b57a470b77a2b91d87a79b765d360d935fad9a5
Divjot Kaur <dkaur50@academic.rrc.ca>
2026-03-13 12:13:17 -0500

    feat: The Subject class has been developed.

---
+++ patterns/observer/subject.py
@@ -0,0 +1,41 @@
+
+
+from abc import ABC, abstractmethod
+
+from .observer import Observer
+
+class Subject(ABC):
+    """This class is responsible for maintaining a list of its 
+    observers and notifying them of state changes or events."""
+
+    def __init__(self):
+        """This function is for initialization."""
+        self._observers = []
+
+    def attach(self, observer: Observer):
+        """This function represents attach method for the observers.
+
+        Returns:
+            observer (Observer): The observer to be added.
+        """
+        if observer not in self._observers:
+            self._observers.append(observer)
+
+    def detach(self, observer: Observer):
+        """This function represents detach method for the observers.
+
+        Returns:
+            observer (Observer): The observer to be removed.
+        """
+
+        if observer in self._observers:
+            self._observers.remove(observer)
+
+    @abstractmethod
+    def notify(self, message: str):
+        """This function is to notify all observers about any change.
+        
+        Returnd:
+            message (str): The message to notifty the observers.
+        """
+        pass
