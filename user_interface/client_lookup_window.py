17836fcb812e3d7b49b8edf6177329b6b7576d22
Divjot Kaur <dkaur50@academic.rrc.ca>
2026-04-03 13:04:43 -0500

    feat: The client_lookup_window.py file has been reviewed and updated with all the required documentation and comments.

--- user_interface/client_lookup_window.py
+++ user_interface/client_lookup_window.py
@@ -14,7 +14,7 @@ from client.client import Client
 
 __author__ = "ACE Faculty"
 __version__ = "1.0.0"
-__credits__ = "<your name here>"
+__credits__ = "Divjot Kaur"
 
 class ClientLookupWindow(LookupWindow):
     """Represents a main window for looking up BankAccounts.""" 
@@ -28,11 +28,18 @@ class ClientLookupWindow(LookupWindow):
         # Calling out the load_data method.
         self.client_listing, self.accounts = load_data()
 
+        # Establishing the three connections here.
         self.lookup_button.clicked.connect(self.on_lookup_client)
         self.client_number_edit.textChanged.connect(self.on_text_changed)
         self.account_table.cellClicked.connect(self.__on_select_account)
 
-    def on_lookup_client(self):
+    def on_lookup_client(self) -> None:
+        """This function is look for the client's account information 
+        using client number.
+        """
+
+        # Obtaining the client number that has been entered into the 
+        # client_number_edit widget.
         client_details = self.client_number_edit.text()
 
         try:
@@ -41,7 +48,7 @@ class ClientLookupWindow(LookupWindow):
             QMessageBox.warning(self, "Input Error", "The client number must be a numeric value.")
             self.reset_display()
             return
-    
+
         if client_number not in self.client_listing:
             QMessageBox.warning(self, "Not Found", f"Client number: {client_number} not found.")
             self.reset_display()
@@ -56,10 +63,15 @@ class ClientLookupWindow(LookupWindow):
         add_row = 0
         
         for account in self.accounts.values():
+            
+            # Below is the case when the client_number matches the key 
+            # in the client_listing dictionary.
             if account.client_number == client_number:
 
                 self.account_table.insertRow(add_row)
 
+                # Creating QTableWidgetItems for each of the 
+                # account_table columns.
                 client_account_number = QTableWidgetItem(str(account.account_number))
                 client_account_number.setTextAlignment(Qt.AlignCenter)
 
@@ -79,17 +91,30 @@ class ClientLookupWindow(LookupWindow):
 
                 add_row += 1
 
+        # Below code ensures none of the table is truncated.
         self.account_table.resizeColumnsToContents()
+    
+    def on_text_changed(self) -> None:
+        """This function would act as a slot for the client_number_edit 
+        textChanged signal
+        """
 
-    def on_text_changed(self):
+        # This would remove all existing rows from the dsiplay, leaving 
+        # the account table with no data displayed.
         self.account_table.setRowCount(0)
 
-    def update_data(self, account: BankAccount):
+    def update_data(self, account: BankAccount) -> None:
+        """This function would update the account information after 
+        there are any changes made to the account balances due to a 
+        transaction.
+        """
 
         for row in range(self.account_table.rowCount()):
 
             if int(self.account_table.item(row, 0).text()) == account.account_number:
                 
+                # This would update the new balance and even align it 
+                # accordingly.
                 balance_item = QTableWidgetItem(f"${account.balance:,.2f}")
                 balance_item.setTextAlignment(Qt.AlignRight | Qt.AlignVCenter)
 
@@ -99,23 +124,22 @@ class ClientLookupWindow(LookupWindow):
 
     @Slot(int, int)
     def __on_select_account(self, row: int, column: int) -> None:
-    
+        """This function acts as a slot for the account_table 
+        cellClicked signal."""
+        
         user_input = self.account_table.item(row, 0)
 
-        if user_input is None:
-            QMessageBox.warning(self, "No Bank Account", "Bank Account selected does not exist.")
+        # This case is possible if the selected account_number does not 
+        # reside in the accounts dictionary.
+        if  user_input is None or account_number_of_client == "":
+            QMessageBox.warning(self, "Invalid Selection", "Please select a valid record.")
             return
-        
-        account_number_of_client = user_input.text().strip()
-            
-        if account_number_of_client == "":
-            QMessageBox.warning(self, "Warning", "Please select a valid record.")
-            return
-
+ 
+        account_number_of_client = user_input.text().strip() 
         account_number = int(account_number_of_client)
 
         if account_number not in self.accounts:
-            QMessageBox.warning(self, "Warning", "Bank Account selected does not exist.")
+            QMessageBox.warning(self, "No Bank Account", "Bank Account selected does not exist.")
             return
 
         account = self.accounts[account_number]
